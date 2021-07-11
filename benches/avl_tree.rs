c6004e5f31df2a3c7e30511da2bb1c5340684363
Taewoo An <taewoo.an@kaist.ac.kr>
2021-07-11 21:14:56 +0900

    Impl bench reference std::BTreeMap

--- benches/avl_tree.rs
+++ benches/avl_tree.rs
@@ -1,12 +1,17 @@
 mod util;
 
 use crate::util::map::bench_sequential;
+use crate::util::map::bench_sequential_reference;
 use cds::tree::avl_tree::AVLTree;
 use criterion::{Criterion, criterion_group, criterion_main};
 
 fn bench_avl_tree(c: &mut Criterion) {
-    bench_sequential::<AVLTree<_, _>>("AVL Tree", 65536, c);
+    bench_sequential::<AVLTree<_, _>>("AVL Tree", 100_000, c);
 }
 
-criterion_group!(avl_tree, bench_avl_tree);
+fn bench_reference_tree(c: &mut Criterion) {
+    bench_sequential_reference(100_000, c);
+}
+
+criterion_group!(avl_tree, bench_avl_tree, bench_reference_tree);
 criterion_main!(avl_tree);
--- benches/util/map.rs
+++ benches/util/map.rs
@@ -1,15 +1,128 @@
-use std::time::{Duration, Instant};
+use std::{collections::BTreeMap, time::{Duration, Instant}};
 
 use cds::map::SequentialMap;
 use criterion::{black_box, Criterion};
 use rand::{prelude::SliceRandom, thread_rng, Rng};
+
+pub fn bench_sequential_reference(already_inserted: u64, c: &mut Criterion)
+{
+    c.bench_function(
+        &format!("{} Inserted std::BTreeMap Insert (batch: 100)", already_inserted),
+        |b| {
+            b.iter_custom(|iters| {
+                let mut map = BTreeMap::new();
+                let mut rng = thread_rng();
+
+                let mut range: Vec<u64> = (0..already_inserted).collect();
+                range.shuffle(&mut rng);
+
+                for i in range.clone() {
+                    let _ = map.insert(i, i.clone());
+                }
+
+                let mut duration: Duration = Duration::ZERO;
+
+                for _ in 0..iters {
+                    let mut keys = Vec::new();
+
+                    for _ in 0..100 {
+                        let mut key: u64 = rng.gen();
+
+                        loop {
+                            if !range.contains(&key) {
+                                break;
+                            }
+
+                            key = rng.gen();
+                        }
+
+                        keys.push(key.clone());
+
+                        let start = Instant::now();
+                        let _ = black_box(map.insert(key, key));
+                        duration += start.elapsed();
+                    }
+
+                    for key in &keys {
+                        map.remove(&key).expect("Error on removing inserted keys");
+                    }
+                }
+
+                duration / 100
+            });
+        },
+    );
+
+    c.bench_function(
+        &format!("{} Inserted std::BTreeMap Lookup", already_inserted),
+        |b| {
+            b.iter_custom(|iters| {
+                let mut map = BTreeMap::new();
+                let mut rng = thread_rng();
+
+                let mut range: Vec<u64> = (0..already_inserted).collect();
+                range.shuffle(&mut rng);
+
+                for i in range {
+                    let _ = map.insert(i, i);
+                }
+
+                let mut duration = Duration::ZERO;
+                for _ in 0..iters {
+                    let key: u64 = rng.gen_range(0..already_inserted);
+
+                    let start = Instant::now();
+                    let _ = black_box(map.get(&key));
+                    duration += start.elapsed();
+                }
+                duration
+            });
+        },
+    );
+
+    c.bench_function(
+        &format!("{} Inserted std::BTreeMap Remove (batch: 100)", already_inserted),
+        |b| {
+            b.iter_custom(|iters| {
+                let mut map = BTreeMap::new();
+                let mut rng = thread_rng();
+
+                let mut range: Vec<u64> = (0..already_inserted).collect();
+                range.shuffle(&mut rng);
+
+                for i in range.clone() {
+                    let _ = map.insert(i, i.clone());
+                }
+
+                let mut duration = Duration::ZERO;
+                for _ in 0..iters {
+                    let keys: Vec<&u64> = range.choose_multiple(&mut rng, 100).collect();
+
+                    for key in &keys {
+                        let start = Instant::now();
+                        let _ = black_box(map.remove(key));
+                        duration += start.elapsed();
+                    }
+
+                    for key in keys {
+                        let key = key.clone();
+                        assert_eq!(map.insert(key, key.clone()), None);
+                    }
+                }
+                duration / 100
+            });
+        },
+    );
+}
+
+
 
 pub fn bench_sequential<M>(name: &str, already_inserted: u64, c: &mut Criterion)
 where
     M: SequentialMap<u64, u64>,
 {
     c.bench_function(
-        &format!("{} Inserted {} Insert", already_inserted, name),
+        &format!("{} Inserted {} Insert (batch: 100)", already_inserted, name),
         |b| {
             b.iter_custom(|iters| {
                 let mut map = M::new();
@@ -83,7 +196,7 @@ where
     );
 
     c.bench_function(
-        &format!("{} Inserted {} Remove", already_inserted, name),
+        &format!("{} Inserted {} Remove (batch: 100)", already_inserted, name),
         |b| {
             b.iter_custom(|iters| {
                 let mut map = M::new();
