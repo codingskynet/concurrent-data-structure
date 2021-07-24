cc4f937aa9d8a42cd2e83762546a11cd4059630c
Taewoo An <taewoo.an@riiid.co>
2021-07-24 13:33:05 +0900

    Add lock AVL tree test

---
+++ tests/avl_tree/lock.rs
@@ -0,0 +1,32 @@
+use cds::{avl_tree::lock::RwLockAVLTree, map::ConcurrentMap};
+use crossbeam_epoch::pin;
+
+use crate::util::map::{stress_concurrent, stress_concurrent_as_sequential};
+
+#[test]
+fn test_rwlock_avl_tree_insert_lookup() {
+    let pin = pin();
+    let avl: RwLockAVLTree<i32, i32> = RwLockAVLTree::new();
+
+    for i in 0..100 {
+        assert_eq!(avl.insert(&i, i, &pin), Ok(()));
+    }
+
+    for i in 0..100 {
+        assert_eq!(avl.insert(&i, i, &pin), Err(i));
+    }
+
+    for i in 0..100 {
+        assert_eq!(avl.lookup(&i, &pin), Some(i));
+    }
+}
+
+#[test]
+fn stress_rwlock_avl_tree_sequential() {
+    stress_concurrent_as_sequential::<String, RwLockAVLTree<_, _>>(50_000);
+}
+
+#[test]
+fn stress_rwlock_avl_tree_concurrent() {
+    stress_concurrent::<String, RwLockAVLTree<_, _>>(64_000, 16);
+}
