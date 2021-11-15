5dc1086dec034f4a770f76c45b9462ceac225f74
Taewoo An <taewoo.an@kaist.ac.kr>
2021-11-15 23:16:08 +0900

    Change per/total on concurrent and use all on vs bench on sequential

--- benches/concurrent.rs
+++ benches/concurrent.rs
@@ -1,4 +1,4 @@
-use criterion::{SamplingMode, Throughput, criterion_main};
+use criterion::{criterion_main, SamplingMode, Throughput};
 
 mod util;
 
@@ -56,22 +56,25 @@ fn bench_mixed_ebstack(c: &mut Criterion) {
 
 const MAP_ALREADY_INSERTED: u64 = 500_000;
 const MAP_PER_OPS: usize = 10_000;
-fn bench_mixed_seqlockavltree(c: &mut Criterion) {
-    let ops_rate = [
-        (100, 0, 0),
-        (0, 100, 0),
-        (0, 0, 100),
-        (5, 90, 5),
-        (30, 50, 20),
-        (50, 0, 50),
-    ];
+const MAP_TOTAL_OPS: usize = 192_000;
 
+const OPS_RATE: [(usize, usize, usize); 7] = [
+    (100, 0, 0),
+    (0, 100, 0),
+    (0, 0, 100),
+    (5, 90, 5),
+    (30, 50, 20),
+    (40, 20, 40),
+    (50, 0, 50),
+];
+
+fn bench_mixed_per_seqlockavltree(c: &mut Criterion) {
     let mut group = c.benchmark_group("SeqLockAVLTree");
     group.sample_size(20);
     group.measurement_time(Duration::from_secs(20));
     group.sampling_mode(SamplingMode::Flat);
 
-    for (insert, lookup, remove) in ops_rate {
+    for (insert, lookup, remove) in OPS_RATE {
         for num in get_test_thread_nums() {
             group.throughput(Throughput::Elements((MAP_PER_OPS * num) as u64));
             bench_mixed_concurrent_map::<SeqLockAVLTree<_, _>>(
@@ -89,11 +92,37 @@ fn bench_mixed_seqlockavltree(c: &mut Criterion) {
     group.finish();
 }
 
+fn bench_mixed_total_seqlockavltree(c: &mut Criterion) {
+    let mut group = c.benchmark_group("SeqLockAVLTree");
+    group.sample_size(20);
+    group.measurement_time(Duration::from_secs(15));
+    group.sampling_mode(SamplingMode::Flat);
+
+    for (insert, lookup, remove) in OPS_RATE {
+        for num in get_test_thread_nums() {
+            group.throughput(Throughput::Elements(MAP_TOTAL_OPS as u64));
+            let per_op = MAP_TOTAL_OPS / num;
+            bench_mixed_concurrent_map::<SeqLockAVLTree<_, _>>(
+                "SeqlockAVLTree",
+                MAP_ALREADY_INSERTED,
+                per_op * insert / 100,
+                per_op * lookup / 100,
+                per_op * remove / 100,
+                num,
+                &mut group,
+            )
+        }
+    }
+
+    group.finish();
+}
+
 criterion_group!(
     bench,
     bench_mixed_treiberstack,
     bench_mixed_ebstack,
-    bench_mixed_seqlockavltree
+    bench_mixed_per_seqlockavltree,
+    bench_mixed_total_seqlockavltree
 );
 criterion_main! {
     bench,
--- benches/sequential.rs
+++ benches/sequential.rs
@@ -1,35 +1,30 @@
 use std::time::Duration;
 
-use criterion::{criterion_main, SamplingMode};
+use criterion::{criterion_main, SamplingMode, Throughput};
 
 mod util;
 
 use cds::{avltree::AVLTree, btree::BTree};
 use criterion::{criterion_group, Criterion};
-use util::sequential::{bench_sequential_map, fuzz_sequential_logs};
+use util::sequential::fuzz_sequential_logs;
 
 use crate::util::sequential::{bench_logs_btreemap, bench_logs_sequential_map};
 
 const MAP_ALREADY_INSERTED: u64 = 500_000;
-const MAP_TOTAL_OPS: usize = 500_000;
+const MAP_TOTAL_OPS: usize = 192_000;
 
-fn bench_btreemap(c: &mut Criterion) {
-    util::sequential::bench_btreemap(1_000_000, c);
-}
+const OPS_RATE: [(usize, usize, usize); 7] = [
+    (100, 0, 0),
+    (0, 100, 0),
+    (0, 0, 100),
+    (5, 90, 5),
+    (30, 50, 20),
+    (40, 20, 40),
+    (50, 0, 50),
+];
 
-fn bench_avltree(c: &mut Criterion) {
-    bench_sequential_map::<AVLTree<_, _>>("AVLTree", 1_000_000, c);
-}
-
-fn bench_btree(c: &mut Criterion) {
-    bench_sequential_map::<BTree<_, _>>("BTree", 1_000_000, c);
-}
-
-fn bench_btree_vs_btreemap(c: &mut Criterion) {
-    let ops_rate = [(10, 80, 10), (20, 40, 20), (30, 50, 20), (40, 20, 40)];
-
-    for (insert, lookup, remove) in ops_rate {
-        println!("Creating logs...");
+fn bench_vs_btreemap(c: &mut Criterion) {
+    for (insert, lookup, remove) in OPS_RATE {
         let logs = fuzz_sequential_logs(
             200,
             MAP_ALREADY_INSERTED,
@@ -45,19 +40,15 @@ fn bench_btree_vs_btreemap(c: &mut Criterion) {
         group.measurement_time(Duration::from_secs(15)); // Note: make almost same the measurement_time to iters * avg_op_time
         group.sampling_mode(SamplingMode::Flat);
         group.sample_size(20);
+        group.throughput(Throughput::Elements(MAP_TOTAL_OPS as u64));
 
         bench_logs_btreemap(logs.clone(), &mut group);
-        bench_logs_sequential_map::<BTree<_, _>>("BTree", logs, &mut group);
+        bench_logs_sequential_map::<BTree<_, _>>("BTree", logs.clone(), &mut group);
+        bench_logs_sequential_map::<AVLTree<_, _>>("AVLTree", logs, &mut group);
     }
 }
 
-criterion_group!(
-    bench,
-    bench_btreemap,
-    bench_avltree,
-    bench_btree,
-    bench_btree_vs_btreemap
-);
+criterion_group!(bench, bench_vs_btreemap);
 criterion_main! {
     bench,
 }
